5f2a6cd4b2e57b9172185c8af5cc75b7dda31c8c
Shirin <shirin.mohajer@gmail.com>
2023-10-03 18:00:08 -0400

    uodated seed data

--- seed/airport.js
+++ seed/airport.js
@@ -1,38 +1,39 @@
 const db = require('../db')
-// const Chance = require('chance')
-const { Airport} = require('../models')
+const { Airport } = require('../models')
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 const createAirport = async () => {
-    const airports = [
-        {
-            name: `Chicago O'Hare`,
-            location: `Chicago, IL`,
-            code: `ORD`
-          },
-          {
-            name: `Atlanta Hartfield-Jackson`,
-            location: `Atlanta, GA`,
-            code: `ATL`
-          },
-          {
-            name: `United BSL`,
-            location: `Basil, France`,
-            code: `BSL`
-          },
-          {
-            name: `New York JFK`,
-            location: `New York, NY`,
-            code: `JFK`
-          },
-    ]
-    await Airport.insertMany(airports)
-    console.log(`created airports`)
+  const airports = [
+    {
+      name: `Chicago O'Hare`,
+      location: `Chicago, IL`,
+      code: `ORD`
+    },
+    {
+      name: `Atlanta Hartfield-Jackson`,
+      location: `Atlanta, GA`,
+      code: `ATL`
+    },
+    {
+      name: `United BSL`,
+      location: `Basil, France`,
+      code: `BSL`
+    },
+    {
+      name: `New York JFK`,
+      location: `New York, NY`,
+      code: `JFK`
+    },
+  ]
+
+  await Airport.insertMany(airports)
+  console.log(`created airports`)
 }
-
-
 
 const run = async () => {
-    await createAirport()
-    db.close()
+  await createAirport()
+  db.close()
 }
+
 run()
--- seed/flights.js
+++ seed/flights.js
@@ -1,47 +1,63 @@
 const db = require('../db')
-// const Chance = require('chance')
 const { Airport, Flight } = require('../models')
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 const createFlight = async () => {
     const airports = await Airport.find()
+
     const flights = [
         {
-            airline: `United`,
+            airline: `Air France`,
             flightNumber: 1234,
             price: 400,
-            numberOfSeats: 100,
-            departingAirport:airports[0]._id,
-            arrivalAirport:airports[1]._id,
+            numberOfSeats: 500,
+            departingAirport: airports[0]._id,
+            arrivalAirport: airports[1]._id,
             departingDateTime: new Date("2023-10-03T00:00:00Z"),
             arrivalDateTime: new Date("2023-10-03T05:30:00Z")
         },
         {
-            airline: `Spirit`,
+            airline: `Sammy's Airline`,
             flightNumber: 234,
             price: 30,
             numberOfSeats: 150,
-            departingAirport:airports[2]._id,
-            arrivalAirport:airports[3]._id,
+            departingAirport: airports[2]._id,
+            arrivalAirport: airports[3]._id,
             departingDateTime: new Date("2023-11-03T00:00:00Z"),
             arrivalDateTime: new Date("2023-12-04T05:30:00Z")
         },
         {
-            airline: `Lufthansa`,
+            airline: `United`,
+            flightNumber: 123,
+            price: 1000,
+            numberOfSeats: 400,
+            departingAirport: airports[1]._id,
+            arrivalAirport: airports[0]._id,
+            departingDateTime: new Date("2023-10-05T00:00:00Z"),
+            arrivalDateTime: new Date("2023-10-06T05:30:00Z")
+        },
+        {
+            airline: `American`,
             flightNumber: 543,
             price: 1000,
             numberOfSeats: 400,
-            departingAirport:airports[1]._id,
-            arrivalAirport:airports[0]._id,
+            departingAirport: airports[1]._id,
+            arrivalAirport: airports[0]._id,
             departingDateTime: new Date("2023-10-05T00:00:00Z"),
             arrivalDateTime: new Date("2023-10-06T05:30:00Z")
         },
     ]
+
     await Flight.insertMany(flights)
     console.log(`created flights`)
-    console.log(flights)
+    // console.log(flights)
+
 }
+
 const run = async () => {
     await createFlight()
     db.close()
 }
+
 run()
