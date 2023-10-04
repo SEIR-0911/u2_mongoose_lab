6454d4e4e24ed14f30b0db4c5b5b7a3a69f19050
sammymarks <sammy.marks@gmail.com>
2023-10-04 05:42:26 -0500

    Initial submission. Had trouble getting console prompts to work with async. Have not yet completed bonus.

--- query.js
+++ query.js
@@ -1,5 +1,18 @@
 const db = require('./db')
+// const prompt = require("prompt-async")
+//https://www.npmjs.com/package/prompt-async
 const { Airport, Flight } = require('./models')
+
+
+const findAll = async () => {
+    const airports = await Airport.find()
+    const flights = await Flight.find()
+
+    console.log(airports, flights)
+}
+
+//User Story 1
+
 
 const userStoryOne = async () => {
     const flights = await Flight.find({}, {airline:1, flightNumber:1, departingDateTime:1, departingAirport:1})
@@ -19,6 +32,9 @@ const userStoryOne = async () => {
 
 
 }
+
+//User Story 2
+
 //     _id: new ObjectId("651c761ed223414737bf4fe5"),
 
 const userStoryTwo = async (myFlightID) => {
@@ -49,13 +65,113 @@ const userStoryTwo = async (myFlightID) => {
     console.log(formattedMyFlight)
 }
 
-// const userStoryThree = async ()
+//User Story 3
 
+
+// const getAirport = () => {
+//     console.log("Enter new airport information")
+//     let airportName = prompt(`Airport Name:`)
+//     let airportLocation = prompt(`Airport Name:`)
+//     let airportCode = prompt(`Airport Code:`)
+//     let airportArray = [airportName,airportLocation,airportCode]
+//     return airportArray
+// }
+
+const userStoryThree = async () => {
+    
+    // prompt.start()
+
+    // const airportName = await prompt.get(`Airport Name:`)
+    // const airportLocation = await prompt.get(`Airport Name:`)
+    // const airportCode = await prompt.get(`Airport Code:`)
+
+    let airportName = `Baltimore Washington International`
+    let airportLocation = `Baltimore, MD`
+    let airportCode = `BWI`
+    
+    const newAirport = await Airport.create({
+        name: `${airportName}`,
+        location: `${airportLocation}`,
+        code: `${airportCode}`
+    })
+    
+
+
+    let newAirline = `United`
+    let newFlightNumber = 123
+    let newPrice = 500
+    let newNumberOfSeats = 600
+    let newDepartingAirportCode = `651c8f33c926127fa52f065b`
+    let newArrivalAirportCode = `651c702fda0debf335ec30bc`
+    let newDepartingDateTime = `2023-10-04T00:01:00`
+    let newArrivalDateTime = `2023-10-04T00:01:00`
+
+    // const newArrivalAirport = await Airport.find({code: newArrivalAirportCode}, {_id:1})
+
+    // const newDepartingAirport = await Airport.find({code: newDepartingAirportCode}, {_id:1})
+
+    const newFlight = await Flight.create({
+        airline: newAirline,
+        flightNumber: newFlightNumber,
+        price: newPrice,
+        numberOfSeats: newNumberOfSeats,
+        departingAirport: newDepartingAirportCode,
+        arrivalAirport: newArrivalAirportCode,
+        departingDateTime: newDepartingDateTime, //
+        arrivalDateTime: newArrivalDateTime //
+    })
+
+    const airports = await Airport.find()
+    console.log(airports)
+
+    const flights = await Flight.find()
+    console.log(flights)
+    
+}
+
+const userStoryFour = async () => {
+    //https://mongoosejs.com/docs/tutorials/findoneandupdate.html
+
+    //Update Airport
+
+    const airportCode = `BSL`
+
+    const aFilter = { code: airportCode }
+    const aUpdate = { name: `EuroAirport Basel Mulhouse Freiburg V3` }
+
+    let updatedAirport = await Airport.findOneAndUpdate(aFilter, aUpdate, {new: true})
+    console.log(updatedAirport)
+
+    //Update Flight
+    const searchAirline = `United`
+    const searchFlightNumber = 1234
+
+    const fFilter = { airline: searchAirline, flightNumber: searchFlightNumber }
+    const fUpdate = { price: 1400 }
+
+    let updatedFlight = await Flight.findOneAndUpdate(fFilter, fUpdate, {new: true})
+    console.log(updatedFlight)
+}
+
+const userStoryFive = async () => {
+    //delete Airport
+    const deleteAirportID = `651d393accfad96112ba7995`
+    await Airport.findByIdAndDelete(deleteAirportID)
+
+    //delete Flight
+    const flightFilter = { price: { $lte: 100 } }
+    await Flight.findOneAndDelete(flightFilter)
+
+}
 
 async function main() {
     try {
         // await userStoryOne()
-        await userStoryTwo("651c761ed223414737bf4fe5")
+        // await userStoryTwo("651c761ed223414737bf4fe5")
+        // await userStoryThree()
+        // await userStoryFour()
+        await userStoryFive()
+        await findAll()
     } catch (error) {
       console.log(error)
     } finally {
